f7a45c409e3eb8af3fc2299339ac4790d808793c
anasely <58348015+anasely@users.noreply.github.com>
2023-11-04 22:53:54 +0100

    add bdd

---
+++ omk/bdd/omk.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 04 nov. 2023 à 21:52
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `omk_thyp_24`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+CREATE TABLE IF NOT EXISTS `resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) DEFAULT NULL,
+  `resource_class_id` int(11) DEFAULT NULL,
+  `resource_template_id` int(11) DEFAULT NULL,
+  `thumbnail_id` int(11) DEFAULT NULL,
+  `title` longtext COLLATE utf8mb4_unicode_ci,
+  `is_public` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  `resource_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_BC91F4167E3C61F9` (`owner_id`),
+  KEY `IDX_BC91F416448CC1BD` (`resource_class_id`),
+  KEY `IDX_BC91F41616131EA` (`resource_template_id`),
+  KEY `IDX_BC91F416FDFF2E92` (`thumbnail_id`),
+  KEY `title` (`title`(190))
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `resource`
+--
+
+INSERT INTO `resource` (`id`, `owner_id`, `resource_class_id`, `resource_template_id`, `thumbnail_id`, `title`, `is_public`, `created`, `modified`, `resource_type`) VALUES
+(1, 1, 23, NULL, NULL, NULL, 1, '2023-10-28 23:21:54', '2023-11-04 20:59:29', 'Omeka\\Entity\\ItemSet'),
+(3, 1, 94, NULL, NULL, 'Usain Bolt', 1, '2023-11-04 20:41:29', '2023-11-04 21:00:58', 'Omeka\\Entity\\Item'),
+(4, 1, 94, 2, NULL, 'Serena Williams', 1, '2023-11-04 20:45:06', '2023-11-04 21:50:31', 'Omeka\\Entity\\Item'),
+(5, 1, 94, 2, NULL, 'Michael Phelps', 1, '2023-11-04 20:48:19', '2023-11-04 21:30:09', 'Omeka\\Entity\\Item');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `resource`
+--
+ALTER TABLE `resource`
+  ADD CONSTRAINT `FK_BC91F41616131EA` FOREIGN KEY (`resource_template_id`) REFERENCES `resource_template` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F416448CC1BD` FOREIGN KEY (`resource_class_id`) REFERENCES `resource_class` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F4167E3C61F9` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `FK_BC91F416FDFF2E92` FOREIGN KEY (`thumbnail_id`) REFERENCES `asset` (`id`) ON DELETE SET NULL;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
