12a0bc4171403c43612d5c797df4cf1370a8aaad
anasely <58348015+anasely@users.noreply.github.com>
2023-11-20 10:08:47 +0100

    omkstats

---
+++ authParams.js
@@ -0,0 +1,6 @@
+export const params = {
+  api: "http://localhost/omk_thyp_24/omeka-s/api/",
+  key: "zCDoVczqJaPXxE2U8WAr0UFYILC0KrNk  ",
+  ident: "SRSzlkuGkcLCePs7CF8AwnRPNt7FvsFR",
+  mail: "anaselyassai@gmail.com",
+};
